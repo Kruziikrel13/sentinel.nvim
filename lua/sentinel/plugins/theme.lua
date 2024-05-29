ea3edf00b40f192bfd4e799fa5eda8aa836bf5a2
Michael Petersen <michael.petersen@cryptsoft.com>
2024-05-30 09:20:24 +1000

    added github theme

---
+++ lua/sentinel/plugins/theme.lua
@@ -0,0 +1,14 @@
+return {
+    'projekt0n/github-nvim-theme',
+    lazy = false,
+    priority = 1000,
+    opts = {
+        dim_inactive = true
+    },
+    config = function(_, opts)
+        local module = require('github-theme')
+        module.setup(opts)
+        module.compile()
+        vim.cmd.colorscheme('github_dark_default')
+    end
+}
