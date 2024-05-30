9b9ca97de05ab9f75993165d69de602a47ca0ec5
Michael Petersen <michael.petersen@cryptsoft.com>
2024-05-30 12:58:32 +1000

    feat: add various editing features

---
+++ lua/sentinel/plugins/editing.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    'numToStr/Comment.nvim',
+    config = true,
+    lazy = false
+  },
+  {
+    'windwp/nvim-autopairs',
+    config = true,
+    event = 'InsertEnter'
+  },
+  {
+    'max397574/better-escape.nvim',
+    config = true
+  }
+}
