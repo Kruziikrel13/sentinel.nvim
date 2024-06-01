9198a06b4485cca4ac8665658ae7ba529fe03070
Michael Petersen <dev@michaelpetersen.io>
2024-06-01 23:01:34 +1000

    feat: add various utils

--- lua/sentinel/plugins/editing.lua
+++ lua/sentinel/plugins/editing.lua
@@ -1,4 +1,3 @@
-local util = require "util"
 return {
   {
     'numToStr/Comment.nvim',
@@ -6,19 +5,69 @@ return {
     lazy = false
   },
   {
-    'windwp/nvim-autopairs',
-    config = function()
-      require('nvim-autopairs').setup()
-      if util.has('nvim-cmp') then
-        local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-        local cmp = require('cmp')
-        cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
-      end
-    end,
-    event = 'InsertEnter'
+    'altermo/ultimate-autopair.nvim',
+    config = true,
+    event = { 'InsertEnter', 'CmdLineEnter' }
   },
   {
     'max397574/better-escape.nvim',
+    config = true
+  },
+  {
+    'kylechui/nvim-surround',
+    config = true
+  },
+  {
+    'lukas-reineke/indent-blankline.nvim',
+    config = function(_, opts)
+      require('ibl').setup(opts)
+    end,
+    opts = {
+      exclude = { filetypes = { 'nvimtree', 'dashboard' } },
+      scope = { enabled = true }
+    },
+    dependencies = { 'HiPhish/rainbow-delimiters.nvim' },
+    main = 'ibl'
+  },
+  {
+    'rmagatti/auto-session',
+    config = true,
+    lazy = false
+  },
+  {
+    'gelguy/wilder.nvim',
+    opts = {
+      modes = {':', '/', '?'}
+    },
+    config = function (_, opts)
+      local wilder = require('wilder')
+      wilder.setup(opts)
+
+      wilder.set_option('renderer', wilder.popupmenu_renderer(
+        wilder.popupmenu_border_theme({
+          highlighter = wilder.basic_highlighter(),
+          max_height = '10%',
+          highlights = {
+            border = 'Normal'
+          },
+          border = 'rounded'
+        })
+      ))
+    end
+  },
+  {
+    'mrjones2014/smart-splits.nvim',
+    config = true
+  },
+  {
+    'yorickpeterse/nvim-window',
+    config = true,
+    keys = {
+      { '<C-w>s', '<cmd>lua require("nvim-window").pick()<cr>', desc = 'Jump Window'}
+    }
+  },
+  {
+    'dstein64/nvim-scrollview',
     config = true
   }
 }
