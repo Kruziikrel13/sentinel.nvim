564c77f26a3c152eac00ae6a7910e8891dcb3f50
Michael Petersen <dev@michaelpetersen.io>
2024-06-01 14:51:04 +1000

    feat: further configure nvimtree

--- lua/sentinel/plugins/explorer.lua
+++ lua/sentinel/plugins/explorer.lua
@@ -1,13 +1,20 @@
 local lazyKeyBind = require('util').lazyKeyBind
-
 return {
-    {
-        'nvim-tree/nvim-tree.lua',
-        config = true,
-        lazy = false,
-        dependencies = { 'nvim-tree/nvim-web-devicons' },
-        keys = {
-            lazyKeyBind('t', '<cmd>NvimTreeToggle<cr>', 'Open Explorer'),
-        }
-    }
+  {
+    'nvim-tree/nvim-tree.lua',
+    opts = {
+      update_focused_file = {
+        enable = true
+      },
+      modified = {
+        enable = true,
+      },
+    },
+    config = true,
+    lazy = false,
+    keys = {
+      lazyKeyBind('t', '<cmd>NvimTreeFindFileToggle<cr>', 'Explorer')
+    },
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+  }
 }
