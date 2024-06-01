67e4feb3a5e4dbc50f639d02a78371fa3ecceaa1
Michael Petersen <dev@michaelpetersen.io>
2024-06-01 14:48:04 +1000

    clean: remove keymap hints

--- lua/sentinel/plugins/tools.lua
+++ lua/sentinel/plugins/tools.lua
@@ -13,34 +13,6 @@ if settings.enable_startuptime then
     }
 end
 
-local keymap_hints = {}
-if settings.enable_keymap_hints then
-    keymap_hints = {
-        'tris203/hawtkeys.nvim',
-        dependencies = { "nvim-lua/plenary.nvim", "nvim-treesitter/nvim-treesitter" },
-        opts = function()
-            local customMaps = {}
-            if Util.has('which-key') then
-                customMaps = {
-                    ['wk.register'] = {
-                        method = 'which_key'
-                    }
-                }
-            else
-                customMaps = {
-                    ['lazy'] = {
-                        method = 'lazy'
-                    }
-                }
-            end
-            return {
-                customMaps = customMaps
-            }
-        end,
-        config = true,
-    }
-end
 return {
     startuptime,
-    keymap_hints
 }
