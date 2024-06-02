9a59d4f185a2c7ddadcc320f8cb1384df318756c
Michael Petersen <michael.petersen@cryptsoft.com>
2024-06-03 06:54:17 +1000

    doc: explain init_file_loading config option

--- lua/configuration.lua
+++ lua/configuration.lua
@@ -10,6 +10,6 @@ conf.maplocalleader = ' '
 conf.enable_startuptime = true
 conf.enable_hardtime = false -- For enforcing good vim practices
 
-conf.init_file_loading = false
+conf.init_file_loading = false -- load .nvim.lua init files (note: we already auto load .lazy.lua files)
 
 return conf
