66cac57da5cf5d2fbb2b35b05341f7c82f9b22ce
Michael Petersen <dev@michaelpetersen.io>
2024-06-02 11:39:24 +1000

    clean: remove mason auto lsp setup
    We will manually setup our lsps per project directory via .lazy.lua

--- lua/sentinel/plugins/lsp.lua
+++ lua/sentinel/plugins/lsp.lua
@@ -10,16 +10,6 @@ return {
       'nvimdev/lspsaga.nvim',
       { 'folke/neodev.nvim', opts = {} },
     },
-    config = function()
-      local completion = require('cmp_nvim_lsp').default_capabilities()
-      require('mason-lspconfig').setup_handlers({
-        function(server_name)
-          require('lspconfig')[server_name].setup {
-            capabilities = completion
-          }
-        end
-      })
-    end
   },
   {
     'folke/neodev.nvim',
