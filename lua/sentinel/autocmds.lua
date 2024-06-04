6d6c6948f30b978104b9ca16c9c1275a5f845869
Michael Petersen <dev@michaelpetersen.io>
2024-06-04 14:14:31 +1000

    fix: add autocmd to fix auto-session and nvim tree incompatibilities

--- lua/sentinel/autocmds.lua
+++ lua/sentinel/autocmds.lua
@@ -1,6 +1,22 @@
+local Util = require('util')
+
 vim.api.nvim_create_autocmd({ 'FocusGained', 'BufEnter', 'CursorHold', 'CursorHoldI'  }, {
   pattern = '*',
   callback = function()
     vim.cmd.checktime()
   end
 })
+
+if Util.has('auto-session') and Util.has('nvim-tree') then
+  vim.api.nvim_create_autocmd({ 'BufEnter' }, {
+    pattern = 'NvimTree*',
+    callback = function()
+      local api = require('nvim-tree.api')
+      local view = require('nvim-tree.view')
+
+      if not view.is_visible() then
+        api.tree.open()
+      end
+    end
+  })
+end
