aa73f702cbc2dc1de2f42450104416548d3f77ff
Michael Petersen <michael.petersen@cryptsoft.com>
2024-06-05 12:15:16 +1000

    repo: use setup lsp function for repo specific lazy config

--- .lazy.lua
+++ .lazy.lua
@@ -1,13 +1,5 @@
 local Util = require('util')
 
-local function setup_language_servers()
-  local lspconfig = require('lspconfig')
-  local capabilities = require('cmp_nvim_lsp').default_capabilities()
-  lspconfig.lua_ls.setup {
-    capabilities = capabilities
-  }
-end
-
-Util.on_plugin_load('nvim-lspconfig', setup_language_servers)
+Util.setup_lsp('lua_ls')
 
 return {}
