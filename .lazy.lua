cdfee0bea86be64045efff76a19070cdf1de5ff3
Michael Petersen <michael.petersen@cryptsoft.com>
2024-06-07 11:01:35 +1000

    repo: manually setup lsp for repo
    temporary measure to just get things going

--- .lazy.lua
+++ .lazy.lua
@@ -1,5 +1,19 @@
 local Util = require('util')
 
-Util.setup_lsp('lua_ls')
+local function lua_ls()
+  local capabilities = vim.tbl_deep_extend('force', {}, vim.lsp.protocol.make_client_capabilities(), require('cmp_nvim_lsp').default_capabilities())
+  require('lspconfig').lua_ls.setup({
+    capabilities = capabilities,
+    settings = {
+      Lua = {
+        workspace = {
+          library = { vim.env.VIMRUNTIME }
+        }
+      }
+    }
+  })
+end
+
+lua_ls()
 
 return {}
