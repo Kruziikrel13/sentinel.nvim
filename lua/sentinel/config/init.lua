f456b92c38d6af097060954ab8501d27047c4c90
Michael Petersen <michael.petersen@cryptsoft.com>
2024-06-07 10:59:08 +1000

    clean: remove unused setup function & format

--- lua/sentinel/config/init.lua
+++ lua/sentinel/config/init.lua
@@ -3,30 +3,19 @@ local M = {}
 M.name = 'sentinel'
 M.version = '02.06.2024'
 
-M.setup = function()
-  vim.uv = vim.uv or vim.loop
-  vim.api.nvim_create_autocmd("User", {
-    group = vim.api.nvim_create_augroup('Sentinel', { clear = true }),
-    pattern = 'VeryLazy',
-    callback = function()
-      -- we load keymaps after everything else is done
-      require('util').load('keymaps')
-    end
-  })
-end
-
 M.did_init = false
 M.init = function()
-    if M.did_init then
-        return
-    end
-    M.did_init = true
+  if M.did_init then
+    return
+  end
+  M.did_init = true
 
-    -- delay notifications until vim.notify was replaced or after 500ms
-    require('util').lazy_notify()
+  -- delay notifications until vim.notify was replaced or after 500ms
+  require('util').lazy_notify()
 
-    require('util').load('options')
-    require('util').load('autocmds')
+  require('util').load('options')
+  require('util').load('autocmds')
+  require('util').load('keymaps')
 end
 
 return M
