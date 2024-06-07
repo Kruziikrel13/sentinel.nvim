b1b703e9d83161c57cfca87c83712810d3c2294c
Michael Petersen <michael.petersen@cryptsoft.com>
2024-06-07 10:59:51 +1000

    chore: run format on theme.lua

--- lua/sentinel/plugins/theme.lua
+++ lua/sentinel/plugins/theme.lua
@@ -1,15 +1,15 @@
 local settings = require('configuration')
 return {
-    'projekt0n/github-nvim-theme',
-    lazy = false,
-    priority = 1000,
-    opts = {
-        dim_inactive = true
-    },
-    config = function(_, opts)
-        local module = require('github-theme')
-        module.setup(opts)
-        module.compile()
-        vim.cmd.colorscheme('github_' .. settings.mode .. '_default')
-    end
+  'projekt0n/github-nvim-theme',
+  lazy = false,
+  priority = 1000,
+  opts = {
+    dim_inactive = true
+  },
+  config = function(_, opts)
+    local module = require('github-theme')
+    module.setup(opts)
+    module.compile()
+    vim.cmd.colorscheme('github_' .. settings.mode .. '_default')
+  end
 }
