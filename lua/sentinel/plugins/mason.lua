9ed299a701fa79ec11aff389249e8b7949a664f4
Michael Petersen <dev@michaelpetersen.io>
2024-06-19 13:59:10 +1000

    format mason

--- lua/sentinel/plugins/mason.lua
+++ lua/sentinel/plugins/mason.lua
@@ -1,9 +1,9 @@
 return {
-    {
-        'williamboman/mason.nvim',
-        build = ':MasonUpdate',
-        cmd = { 'Mason', 'MasonUpdate', 'MasonInstall', 'MasonUninstall', 'MasonUninstallAll', 'MasonLog' },
-        optional = true,
-        config = true,
-    }
+  {
+    'williamboman/mason.nvim',
+    build = ':MasonUpdate',
+    cmd = { 'Mason', 'MasonUpdate', 'MasonInstall', 'MasonUninstall', 'MasonUninstallAll', 'MasonLog' },
+    optional = true,
+    config = true,
+  }
 }
