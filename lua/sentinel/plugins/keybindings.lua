e1fb06df8632739ce01c07445057acedc8d308a0
Michael Petersen <dev@michaelpetersen.io>
2024-06-19 13:34:19 +1000

    keys: move key mappings to legendary

--- lua/sentinel/plugins/keybindings.lua
+++ lua/sentinel/plugins/keybindings.lua
@@ -1,5 +1,14 @@
-local whichKeyGroup = require('util').whichKeyGroup
-local lazyKeyBind = require('util').lazyKeyBind
+local whichKeyGroup = require('utils.keys').whichKeyGroup
+
+local keymap_fns = {
+  telescope = {}
+}
+
+function keymap_fns.telescope.live_grep()
+  require('telescope.builtin').live_grep()
+end
+
+
 return {
   {
     'folke/which-key.nvim',
@@ -7,6 +16,7 @@ return {
     opts = {
       keybind_groups = {
         ['<leader>x'] = whichKeyGroup('Utilities'),
+        ['<leader>f'] = whichKeyGroup('Finder'),
         ['<leader>g'] = whichKeyGroup('Git'),
         ['<Tab>'] = whichKeyGroup('Language Server')
       }
@@ -30,6 +40,13 @@ return {
         },
         lazy_nvim = true,
         nvim_tree = true
+      },
+      keymaps = {
+        { '<leader>xL', '<cmd>Lazy<cr>', description = 'Lazy'},
+        { '<C-s>', '<cmd>w<cr>', description = 'Save File'},
+        { '<C-q>', '<cmd>qa!<cr>', description = 'Quit All'},
+        { '00', '^', opts = { silent = true, noremap = true }},
+        { '<leader>ff', keymap_fns.telescope.live_grep, description = 'Telescope: Live Grep'}
       }
     }
   }
