9aafb739e7de159216fc6487cd2292a82295d99f
Michael Petersen <dev@michaelpetersen.io>
2024-06-19 13:46:49 +1000

    more autocmd additions

--- lua/sentinel/autocmds.lua
+++ lua/sentinel/autocmds.lua
@@ -1,22 +1,48 @@
-local Util = require('util')
-
-vim.api.nvim_create_autocmd({ 'FocusGained', 'BufEnter', 'CursorHold', 'CursorHoldI'  }, {
-  pattern = '*',
+vim.api.nvim_create_autocmd({ 'FocusGained', 'TermClose', 'TermLeave' }, {
   callback = function()
     vim.cmd.checktime()
   end
 })
 
-if Util.has('auto-session') and Util.has('nvim-tree') then
-  vim.api.nvim_create_autocmd({ 'BufEnter' }, {
-    pattern = 'NvimTree*',
-    callback = function()
-      local api = require('nvim-tree.api')
-      local view = require('nvim-tree.view')
+vim.api.nvim_create_autocmd("TextYankPost", {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
 
-      if not view.is_visible() then
-        api.tree.open()
-      end
+vim.api.nvim_create_autocmd({ "VimResized" }, {
+  callback = function()
+    local current_tab = vim.fn.tabpagenr()
+    vim.cmd("tabdo wincmd =")
+    vim.cmd("tabnext " .. current_tab)
+  end,
+})
+
+vim.api.nvim_create_autocmd({ 'BufEnter' }, {
+  pattern = 'NvimTree*',
+  callback = function()
+    local api = require('nvim-tree.api')
+    local view = require('nvim-tree.view')
+
+    if not view.is_visible() then
+      api.tree.open()
     end
-  })
-end
+  end
+})
+
+-- disable hlargs if lsp server has same capabilities 
+vim.api.nvim_create_augroup('LspAttach_hlargs', {clear = true})
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = 'LspAttach_hlargs',
+  callback = function(args)
+    if not (args.data and args.data.client_id) then
+      return
+    end
+
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    local caps = client.server_capabilities
+    if caps.semanticTokensProvider and caps.semanticTokensProvider.full then
+      require("hlargs").disable_buf(args.buf)
+    end
+  end,
+})
