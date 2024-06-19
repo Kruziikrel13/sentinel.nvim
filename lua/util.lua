d9bffcd9fabb174095db3b246bcb470067eb73cf
Michael Petersen <dev@michaelpetersen.io>
2024-06-19 11:45:04 +1000

    utils: rewrite getCapabilities fn

--- lua/util.lua
+++ lua/util.lua
@@ -40,6 +40,8 @@ M.setup_lsp = function(lsp)
       capabilities = capabilities
     }
   end
+M.getCapabilities = function ()
+  return vim.tbl_deep_extend('force', {}, vim.lsp.protocol.make_client_capabilities(), require('cmp_nvim_lsp').default_capabilities())
 end
 
 --- @param fn function
