f3b66d1087ae31d9c5866db697ec017d6a5c6d64
Michael Petersen <dev@michaelpetersen.io>
2024-06-19 11:45:47 +1000

    utils: add plugin utilities

--- lua/utils/plugins.lua
+++ lua/utils/plugins.lua
@@ -8,4 +8,24 @@ function M.has(name)
   return M.getPlugin(name) ~= nil
 end
 
+function M.isLoaded(name)
+  local Config = require('lazy.core.config')
+  return Config.plugins[name] and Config.plugins[name]._.loaded
+end
+
+function M.onLoad(name, fn)
+  if M.isLoaded(name) then
+    fn(name)
+  else
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'LazyLoad',
+      callback = function(event)
+        if event.data == name then
+          fn(name)
+        end
+      end
+    })
+  end
+end
+
 return M
