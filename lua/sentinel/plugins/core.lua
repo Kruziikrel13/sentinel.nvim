74af9f4dc1ac456d1e39cb73861208a39b03a8aa
Michael Petersen <dev@michaelpetersen.io>
2024-06-19 13:59:03 +1000

    add activate plugin helper

--- lua/sentinel/plugins/core.lua
+++ lua/sentinel/plugins/core.lua
@@ -1,3 +1,9 @@
 require('sentinel.config').init()
-
-return { 'folke/lazy.nvim' }
+local lazyKeyBind = require('utils.keys').lazyKeyBind
+return {
+  'folke/lazy.nvim',
+  {
+    'roobert/activate.nvim',
+    dependencies = { 'nvim-telescope/telescope.nvim', dependencies = { 'nvim-lua/plenary.nvim' } }
+  }
+}
