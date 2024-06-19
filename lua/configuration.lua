145dfd9dc932b1c573f82d9d3785ab03163ad8c9
Michael Petersen <dev@michaelpetersen.io>
2024-06-19 13:46:42 +1000

    add freeze config option

--- lua/configuration.lua
+++ lua/configuration.lua
@@ -9,9 +9,11 @@ conf.maplocalleader = ' '
 
 conf.enable_startuptime = true
 conf.enable_hardtime = false -- For enforcing good vim practices
+conf.enable_freeze = false -- Freeze Code / Convert code selection to image
 
 -- lspsaga | navigator | combined | none
 conf.lsp_helper = 'combined'
+
 
 conf.init_file_loading = false -- load .nvim.lua init files (note: we already auto load .lazy.lua files)
 
