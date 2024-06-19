bd3739dd772a9a9b07aa60e31058769b8eab9258
Michael Petersen <dev@michaelpetersen.io>
2024-06-19 13:46:25 +1000

    properly configure explorer

--- lua/sentinel/plugins/explorer.lua
+++ lua/sentinel/plugins/explorer.lua
@@ -1,20 +1,51 @@
-local lazyKeyBind = require('util').lazyKeyBind
+local lazyKeyBind = require('utils.keys').lazyKeyBind
+
+local function bindOnAttach(bufnr)
+  local api = require('nvim-tree.api')
+  api.config.mappings.default_on_attach(bufnr)
+
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  vim.keymap.set('n', '>', api.tree.change_root_to_node, opts('change root to node'))
+  vim.keymap.set('n', '<', api.tree.change_root_to_parent, opts('change root to parent'))
+  vim.keymap.set('n', '<BS>', api.tree.toggle_hidden_filter, opts('toggle hidden'))
+  vim.keymap.set('n', 'C', api.node.navigate.parent_close, opts('close parent'))
+end
+
 return {
   {
     'nvim-tree/nvim-tree.lua',
     opts = {
-      update_focused_file = {
-        enable = true
-      },
+      on_attach = bindOnAttach,
+      hijack_cursor = true,
+      disable_netrw = true,
+      prefer_startup_root = true,
+      sync_root_with_cwd = true,
       modified = {
         enable = true,
       },
+      renderer = {
+        group_empty = true,
+      },
+      diagnostics = {
+        enable = true
+      },
+      filters = {
+        dotfiles = true
+      }
     },
     config = true,
     lazy = false,
     keys = {
-      lazyKeyBind('t', '<cmd>NvimTreeFindFileToggle<cr>', 'Explorer')
+      lazyKeyBind('t', '<cmd>NvimTreeToggle<cr>', 'Explorer'),
     },
     dependencies = { 'nvim-tree/nvim-web-devicons' },
+  },
+  {
+    'antosha417/nvim-lsp-file-operations',
+    config = true,
+    dependencies = { 'nvim-tree/nvim-tree.lua', 'nvim-lua/plenary.nvim' }
   }
 }
