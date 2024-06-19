d1f70d6f6880bb2db2ac0f7ecd2f0d4938d1c497
Michael Petersen <dev@michaelpetersen.io>
2024-06-19 11:41:12 +1000

    repo: use lazy dev with lua_ls for repo config

--- .lazy.lua
+++ .lazy.lua
@@ -1,19 +1,30 @@
 local Util = require('util')
-
-local function lua_ls()
-  local capabilities = vim.tbl_deep_extend('force', {}, vim.lsp.protocol.make_client_capabilities(), require('cmp_nvim_lsp').default_capabilities())
-  require('lspconfig').lua_ls.setup({
-    capabilities = capabilities,
-    settings = {
-      Lua = {
-        workspace = {
-          library = { vim.env.VIMRUNTIME }
-        }
+return {
+  {
+    'folke/lazydev.nvim',
+    ft = 'lua',
+    opts = {
+      library = {
+        { path = "luvit-meta/library", words = { "vim%.uv" } },
+      }
+    },
+    config = function(_, opts)
+      require("lazydev").setup(opts)
+      Util.setupLspServers('lua_ls')
+    end,
+    dependencies = {
+      { "Bilal2453/luvit-meta", lazy = true },
+      {
+        "hrsh7th/nvim-cmp",
+        optional = true,
+        opts = function(_, opts)
+          opts.sources = opts.sources or {}
+          table.insert(opts.sources, {
+            name = "lazydev",
+            group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+          })
+        end,
       }
     }
-  })
-end
-
-lua_ls()
-
-return {}
+  }
+}
