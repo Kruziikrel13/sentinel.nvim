f02993f70e2c34a683e8d89b625e4c953c23f1b5
Michael Petersen <dev@michaelpetersen.io>
2024-06-19 13:46:34 +1000

    various editing additions

--- lua/sentinel/plugins/editing.lua
+++ lua/sentinel/plugins/editing.lua
@@ -1,5 +1,18 @@
-local Util = require('util')
-return {
+local lazyKeyBind = require('utils.keys').lazyKeyBind
+local settings = require('configuration')
+
+local freeze = {}
+if settings.enable_freeze then
+  freeze = {
+    'AlejandroSuero/freeze-code.nvim',
+    config = true,
+    keys = {
+      lazyKeyBind('<leader>xf', '<cmd>Freeze<cr>', 'Freeze Code')
+    }
+  }
+end
+
+return { freeze,
   {
     'numToStr/Comment.nvim',
     config = true,
@@ -8,7 +21,10 @@ return {
   {
     'altermo/ultimate-autopair.nvim',
     config = true,
-    event = { 'InsertEnter', 'CmdLineEnter' }
+    event = 'InsertEnter'
+  },
+  {
+    'mrcjkb/nvim-lastplace'
   },
   {
     'max397574/better-escape.nvim',
@@ -28,8 +44,7 @@ return {
   {
     'rmagatti/auto-session',
     opts = {
-      auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
-      pre_save_cmds = { 'NvimTreeClose' },
+      auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" }
     },
     config = function (_, opts)
       vim.o.sessionoptions = 'blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions'
@@ -39,6 +54,35 @@ return {
   },
   {
     'dstein64/nvim-scrollview',
+    config = true
+  },
+  {
+    'sitiom/nvim-numbertoggle'
+  },
+  {
+    'LintaoAmons/scratch.nvim',
+    opts = {
+      window_cmd = 'tabedit'
+    },
+    keys = {
+      lazyKeyBind('<leader>xs', '<cmd>Scratch<cr>', 'Scratch Window')
+    }
+  },
+  {
+    'ChuufMaster/buffer-vacuum',
+    config = true
+  },
+  {
+    'echasnovski/mini.cursorword',
+    config = true
+  },
+  {
+    'nanozuki/tabby.nvim',
+    config = true,
+    dependencies = { 'nvim-tree/nvim-web-devicons' }
+  },
+  {
+    'folke/twilight.nvim',
     config = true
   }
 }
