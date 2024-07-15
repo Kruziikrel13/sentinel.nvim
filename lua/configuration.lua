d7f57494790f417bc8bae6d80c40ff445059e2ac
Michael Petersen <dev@michaelpetersen.io>
2024-07-15 15:51:02 +1000

    remove unused config settings

--- lua/configuration.lua
+++ lua/configuration.lua
@@ -11,12 +11,6 @@ conf.enable_startuptime = false -- startuptime monitoring
 conf.enable_fugit = false -- fugit
 conf.enable_hardtime = false -- For enforcing good vim practices
 
----@type 'lspsaga' | 'navigator' | 'combined' | nil
-conf.lsp_helper = 'combined'
-
----@type 'lsp_lines' | 'diagflow' | nil
-conf.diagnostics = 'lsp_lines'
-
 conf.init_file_loading = false -- load .nvim.lua init files (note: we already auto load .lazy.lua files)
 
 conf.mode = 'dark' -- unused currently
