6128648d45430ee7791b256fe8672c7a65174f86
Kruziikrel13 <dev@michaelpetersen.io>
2024-08-02 13:59:41 +1000

    add flow theme and transparency config option

--- lua/configuration.lua
+++ lua/configuration.lua
@@ -7,9 +7,11 @@ conf.namespace = 'sentinel'
 conf.mapleader = ' '
 conf.maplocalleader = ' '
 
+-- Plugins
 conf.enable_startuptime = false -- startuptime monitoring
-conf.enable_fugit = false -- fugit
+conf.enable_fugit = false -- experimental git plugin
 conf.enable_hardtime = false -- For enforcing good vim practices
+conf.enable_timespent = true
 
 ---@type "lsp_lines" | "tiny-inline"
 conf.line_diagnostics = 'lsp_lines'
@@ -19,6 +21,9 @@ conf.lsp_actions = 'clear-action'
 
 conf.init_file_loading = false -- load .nvim.lua init files (note: we already auto load .lazy.lua files)
 
-conf.mode = 'dark' -- unused currently
+---@type "flow" | "github"
+conf.theme = "github"
+conf.enable_transparency = false -- enables transparency if theme supports it
+conf.mode = 'dark'
 
 return conf
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,15 +1,41 @@
 local settings = require('configuration')
-return {
-  'projekt0n/github-nvim-theme',
-  lazy = false,
-  priority = 1000,
-  opts = {
-    dim_inactive = true
-  },
-  config = function(_, opts)
-    local module = require('github-theme')
-    module.setup(opts)
-    module.compile()
-    vim.cmd.colorscheme('github_' .. settings.mode .. '_default')
-  end
-}
+
+local theme = {}
+if settings.theme == 'flow'then
+  theme = {
+    '0xstepit/flow.nvim',
+    lazy = false,
+    priority = 1000,
+    opts = {
+      transparent = settings.enable_transparency,
+      fluo_color = 'orange',
+      mode = 'normal',
+      aggressive_spell = false
+    },
+    config = function (_, opts)
+      require('flow').setup(opts)
+      vim.cmd.colorscheme('flow')
+    end
+  }
+elseif settings.theme == 'github' then
+  theme = {
+    'projekt0n/github-nvim-theme',
+    lazy = false,
+    priority = 1000,
+    opts = {
+      options = {
+        dim_inactive = true,
+        transparent = settings.enable_transparency
+      }
+    },
+    config = function(_, opts)
+      local github = require('github-theme')
+      github.setup(opts)
+      github.compile(false)
+      vim.cmd.colorscheme('github_' .. settings.mode .. '_default')
+    end
+
+  }
+end
+
+return theme
