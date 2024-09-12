07ce9fa35e84cf1762d730df7269760cf49a5bc7
Michael Petersen <dev@michaelpetersen.io>
2024-09-12 12:13:30 +1000

    check if anything was selected in templates before proceeding

--- lua/helpers/templates.lua
+++ lua/helpers/templates.lua
@@ -16,6 +16,9 @@ function M.writeTemplate()
   local Utils = require('helpers.utils')
   local keylist = Utils.table_keys(M.templates)
   Utils.select(keylist, 'Select Template', function(selection)
+    if selection == nil then
+      return
+    end
     local absolute_path = Utils.find_root() .. '/.lazy.lua'
     if vim.fn.filereadable(absolute_path) == 1 then
       Utils.select({ 'yes', 'no' }, 'Overwrite Existing?', function(confirmation)
