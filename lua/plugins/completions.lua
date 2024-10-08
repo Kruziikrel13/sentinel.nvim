18bb0b21e1c952cb77b699a6494a9443a4389018
Michael Petersen <dev@michaelpetersen.io>
2024-10-08 15:07:19 +1000

    completions: set keybinds for blink completion

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -64,7 +64,16 @@ if require('configuration').experimental.blink then
       highlight = {
         use_nvim_cmp_as_default = true,
       },
-      nerd_font_variant = 'mono'
+      nerd_font_variant = 'mono',
+      keymap = {
+        show = {},
+        hide = {},
+        accept = '<A-CR>',
+        select_prev = { '<Up>', '<A-k>' },
+        select_next = { '<Down>', '<A-j>' }
+      },
+      trigger = { signature_help = { enabled = true } },
+      accept = { auto_brackets = { enabled = true } }
     }
   }
 end
