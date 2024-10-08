b68d6baf6ab205700271c81b074d999bcade2f96
Michael Petersen <dev@michaelpetersen.io>
2024-10-08 17:11:46 +1000

    refactor: re-organize config

--- init.lua
+++ init.lua
@@ -1,11 +1,7 @@
-local configuration = require('configuration')
+vim.uv = vim.uv or vim.loop
+vim.loader.enable()
 
-vim.loader.enable();
+vim.g.mapleader = require('configuration').mapleader
+vim.g.maplocalleader = require('configuration').maplocalleader
 
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-vim.g.mapleader = configuration.mapleader
-vim.g.maplocalleader = configuration.maplocalleader
-
--- Entry point, during lazy loading it also executes the rest of our configuration files
-require('core.lazy')
+require('init').setup()
