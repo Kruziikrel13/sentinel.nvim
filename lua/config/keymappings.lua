e5053a1cc578df094decd0a3ae13fe034c4ee2ca
Michael Petersen <dev@michaelpetersen.io>
2024-10-26 14:33:52 +1000

    add keybind for theme switching

--- lua/config/keymappings.lua
+++ lua/config/keymappings.lua
@@ -2,6 +2,6 @@ vim.keymap.set('n', '00', '^', { silent = true })
 vim.keymap.set('n', '0', '0', { silent = true })
 vim.keymap.set('t', [[<C-\>]], [[<C-\><C-n>]], { silent = true })
 
-vim.keymap.set('n', '<leader>xt', function()
+vim.keymap.set('n', '<leader>xT', function()
   require('helpers.templates').writeTemplate()
 end, { silent = true, desc = "Generate Template" })
