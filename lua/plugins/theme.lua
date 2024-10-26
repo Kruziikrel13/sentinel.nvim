240addf766e982785cebcef8a57baad34bc40399
Michael Petersen <dev@michaelpetersen.io>
2024-10-26 14:33:35 +1000

    use themery for theme switching

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,54 +1,43 @@
 local configuration = require('configuration')
 
-local theme = {}
-if configuration.theme.preset == 'flow'then
-  theme = {
-    '0xstepit/flow.nvim',
-    lazy = false,
-    priority = 1000,
-    opts = {
-      transparent = configuration.theme.transparency,
-      fluo_color = 'orange',
-      mode = 'normal',
-      aggressive_spell = false
-    },
-    config = function (_, opts)
-      require('flow').setup(opts)
-      vim.cmd.colorscheme('flow')
-    end
-  }
-elseif configuration.theme.preset == 'github' then
-  theme = {
-    'projekt0n/github-nvim-theme',
-    lazy = false,
-    priority = 1000,
-    opts = {
-      options = {
-        dim_inactive = true,
-        transparent = configuration.theme.transparency
+return {
+  'zaldih/themery.nvim',
+  lazy = false,
+  opts = {
+    themes = {
+      {
+        name = "GitHub Dark",
+        colorscheme = "github_dark_default",
+        after = [[
+            vim.opt.background = "dark"
+          ]]
+      },
+      {
+        name = "GitHub Light",
+        colorscheme = "github_light_default",
+        after = [[
+            vim.opt.background = "light"
+          ]]
       }
-    },
-    config = function(_, opts)
-      local github = require('github-theme')
-      github.setup(opts)
-      github.compile(false)
-      vim.cmd.colorscheme('github_' .. configuration.theme.mode .. '_default')
-    end
-
-  }
-elseif configuration.theme.preset == 'serene' then
-  theme = {
-    'samharju/serene.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      if configuration.theme.transparency then
-        vim.cmd.colorscheme('serene-transparent')
-      else
-        vim.cmd.colorscheme('serene')
+    }
+  },
+  keys = {
+    { '<leader>xt', '<CMD>Themery<CR>', desc = "Set Theme" }
+  },
+  dependencies = {
+    {
+      'projekt0n/github-nvim-theme',
+      opts = {
+        options = {
+          dim_inactive = true,
+          transparent = configuration.theme.transparency
+        }
+      },
+      config = function(_, opts)
+        local github = require('github-theme')
+        github.setup(opts)
+        github.compile(false)
       end
-    end
+    }
   }
-end
-
-return theme
+}
