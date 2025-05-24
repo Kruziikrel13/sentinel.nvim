fdeeae52b6a34c364c78fea6a4eca414dff344ed
Kruziikrel13 <dev@michaelpetersen.io>
2025-05-24 11:00:37 +1000

    remove enables

--- init.lua
+++ init.lua
@@ -5,5 +5,3 @@ vim.g.mapleader = require('configuration').mapleader
 vim.g.maplocalleader = require('configuration').maplocalleader
 
 require('init').plugin_setup()
-
-vim.lsp.enable({'nixd', 'lua_ls', 'ts_ls'})
