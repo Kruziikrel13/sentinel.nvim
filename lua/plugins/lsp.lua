1ec5d1c4b82811988b49b624b299e64d299aa728
Kruziikrel13 <dev@michaelpetersen.io>
2025-06-10 16:05:30 +1000

    cleanup lsp plugins
    will redo functionality

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -15,43 +15,4 @@ return {
     opts = {},
     dependencies = { 'nvim-telescope/telescope-fzf-native.nvim', optional = true }
   },
-  {
-    "Dan7h3x/signup.nvim",
-    branch = "main",
-    opts = {}
-  },
-  {
-    'nvimdev/lspsaga.nvim',
-    opts = {
-      breadcrumbs = {
-        enable = false
-      },
-      finder = {
-        keys = {
-          vsplit = "<C-v>",
-          split = "<C-x>",
-          toggle_or_open = "<CR>"
-        }
-      },
-      rename = {
-        keys = {
-          quit = '<Esc>',
-          select = 'e'
-        }
-      },
-      lightbulb = {
-        sign = false
-      }
-    },
-    keys = {
-      { '<Tab><Tab>', '<cmd>Lspsaga hover_doc<cr>',
-        desc = 'Hover Doc' },
-      { '<Tab>r', '<cmd>Lspsaga rename ++project<cr>',
-        desc = 'Rename' },
-      { '<Tab>d', '<cmd>Lspsaga peek_definition<cr>',
-        desc = 'Peek' },
-      { '<Tab>f', '<cmd>Lspsaga finder<cr>',
-        desc = 'Find' }
-    }
-  },
-}
+ }
