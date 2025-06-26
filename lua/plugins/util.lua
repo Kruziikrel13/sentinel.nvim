f5cb04bddc31171612f44c60fa49801fa282def0
kruziikrel13 <dev@michaelpetersen.io>
2025-06-26 23:50:22 +1000

    terminal: use floaterm

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -1,12 +1,3 @@
--- Terminal Mappings
-local function term_nav(dir)
-  ---@param self snacks.terminal
-  return function(self)
-    return self:is_floating() and "<c-" .. dir .. ">" or vim.schedule(function()
-      vim.cmd.wincmd(dir)
-    end)
-  end
-end
 
 return {
   {
@@ -14,16 +5,6 @@ return {
     opts = {
       bigfile = { enabled = true },
       quickfile = { enabled = true },
-      terminal = {
-        win = {
-          keys = {
-            nav_h = { "<C-h>", term_nav("h"), desc = "Go to Left Window", expr = true, mode = "t" },
-            nav_j = { "<C-j>", term_nav("j"), desc = "Go to Lower Window", expr = true, mode = "t" },
-            nav_k = { "<C-k>", term_nav("k"), desc = "Go to Upper Window", expr = true, mode = "t" },
-            nav_l = { "<C-l>", term_nav("l"), desc = "Go to Right Window", expr = true, mode = "t" },
-          },
-        }
-      }
     },
     keys = {
       { '<leader>.', function() Snacks.scratch() end, desc = 'Toggle Scratch Buffer' },
@@ -46,7 +27,17 @@ return {
       { "<leader>qd", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
     },
   },
-
+  {
+    'nvzone/floaterm',
+    dependencies = 'nvzone/volt',
+    opts = {},
+    cmd = 'FloatermToggle',
+    keys = {
+      { [[<C-\><C-\>]], function()
+        require('floaterm').toggle()
+      end, desc = 'Terminal', mode = { 'n', 't' } }
+    }
+  },
   -- library used by other plugins
   { "nvim-lua/plenary.nvim", lazy = true },
 }
