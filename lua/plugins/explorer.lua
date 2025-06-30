cb396cf32000e75ed27a3298c6c9250541472f36
kruziikrel13 <dev@michaelpetersen.io>
2025-06-30 15:16:25 +1000

    explorer: added bindings for explorer

--- lua/plugins/explorer.lua
+++ lua/plugins/explorer.lua
@@ -2,6 +2,25 @@ return {
   'kruziikrel13/snacks.nvim',
   opts = {
     explorer = { enable = true  }
+    picker = {
+      sources = {
+        explorer = {
+          matcher = { fuzzy = true },
+          win = {
+            list = {
+              keys = {
+                ['<BS>'] = 'toggle_hidden',
+                ['C'] = 'explorer_close_all',
+                ['E'] = 'explorer_open_all',
+                ['c'] = 'explorer_close',
+                ['<'] = 'explorer_up',
+                ['>'] = 'explorer_focus'
+              }
+            },
+          },
+        }
+      }
+    }
   },
   keys = {
     { 't', function()
