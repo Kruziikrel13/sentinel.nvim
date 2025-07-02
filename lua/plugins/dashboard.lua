9d13cbad9d7fcd247e099f92e84f655da386debf
kruziikrel13 <dev@michaelpetersen.io>
2025-07-02 18:16:40 +1000

    dashboard: refactored dashboard section definitions

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -43,23 +43,50 @@ return {
 					{ section = "keys", gap = 1, padding = 1 },
 				},
 				function()
-					local in_git = Snacks.git.get_root() ~= nil
-					local gh_available = vim.fn.executable("gh") == 1
-
-					if not in_git or not gh_available then
+					if not Snacks.git.get_root() then
 						return
 					end
 
-					local sections = {
+					local gh = {
 						{
-							section = "terminal",
-							cmd = "gh contribs -W",
+							title = "Notifications",
+							indent = 3,
 							height = 5,
-							enabled = gh_available,
+							key = "N",
+							ttl = 5 * 60,
+							cmd = "gh notify -s -n5",
+							action = function()
+								vim.ui.open("https://github.com/notifications")
+							end,
 						},
 						{
+							title = "Open Issues",
+							indent = 3,
+							height = 7,
+							key = "I",
+							ttl = 5 * 60,
+							cmd = "gh issue list -L 3",
+							action = function()
+								vim.fn.jobstart("gh issue list --web", { detach = true })
+							end,
+						},
+						{
+							title = "Open PRs",
+							indent = 3,
+							height = 7,
+							key = "P",
+							ttl = 5 * 60,
+							cmd = "gh pr list -L 3",
+							action = function()
+								vim.fn.jobstart("gh pr list --web", { detach = true })
+							end,
+						},
+					}
+
+					local sections = {
+						{
 							icon = "",
-							desc = "Browse Repo",
+							title = "Browse Repo",
 							key = "b",
 							action = function()
 								Snacks.gitbrowse({
@@ -68,63 +95,29 @@ return {
 							end,
 						},
 						{
-							section = "terminal",
-							ttl = 5 * 60,
-							icon = "",
-							title = "Notifications",
-							indent = 3,
-							height = 5,
-							key = "N",
-							cmd = "gh notify -s -n5",
-							action = function()
-								vim.ui.open("https://github.com/notifications")
-							end,
-							enabled = gh_available,
-						},
-						{
-							section = "terminal",
-							ttl = 5 * 60,
-							icon = "",
-							title = "Open Issues",
-							indent = 3,
-							height = 7,
-							key = "i",
-							cmd = "gh issue list -L 3",
-							action = function()
-								vim.fn.jobstart("gh issue list --web", { detach = true })
-							end,
-							enabled = in_git and gh_available,
-						},
-						{
-							section = "terminal",
-							ttl = 5 * 60,
-							icon = "",
-							title = "Open PRs",
-							indent = 3,
-							height = 7,
-							key = "P",
-							cmd = "gh pr list -L 3",
-							action = function()
-								vim.fn.jobstart("gh pr list --web", { detach = true })
-							end,
-							enabled = in_git and gh_available,
-						},
-						{
-							section = "terminal",
-							ttl = 5 * 60,
 							icon = "",
 							title = "Git Status",
 							indent = 3,
-							height = 10,
+							height = 5,
+							ttl = 5 * 60,
 							cmd = "git --no-pager diff --stat -B -M -C",
+							section = "terminal",
 						},
 					}
+
+					if vim.fn.executable("gh") == 1 then
+						gh = vim.tbl_map(function(cmd)
+							return vim.tbl_extend("keep", cmd, {
+								section = "terminal",
+							})
+						end, gh)
+						table.insert(sections, gh)
+					end
 
 					return vim.tbl_map(function(cmd)
 						return vim.tbl_extend("keep", cmd, {
 							pane = 2,
 							padding = 1,
-							enabled = in_git,
 						})
 					end, sections)
 				end,
@@ -137,7 +130,7 @@ return {
 		{
 			"<leader>D",
 			function()
-				Snacks.dashboard()
+				Snacks.dashboard.open()
 			end,
 			desc = "Dashboard",
 		},
