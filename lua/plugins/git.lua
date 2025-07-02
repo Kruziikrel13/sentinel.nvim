bba07cc29e1acbb58fc4496a62b3041a132f13e8
kruziikrel13 <dev@michaelpetersen.io>
2025-07-02 16:30:55 +1000

    lazygit: only enable lazygit if it's present on system path

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,9 +1,8 @@
-return {
-	{
+local lazygit_spec = {}
+if vim.fn.executable("lazygit") == 1 then
+	lazygit_spec = {
 		"kruziikrel13/snacks.nvim",
-		opts = {
-			lazygit = { enable = true },
-		},
+		opts = { lazygit = { enable = true } },
 		keys = {
 			{
 				"<leader>gg",
@@ -13,7 +12,10 @@ return {
 				desc = "LazyGit",
 			},
 		},
-	},
+	}
+end
+return {
+	lazygit_spec,
 	{
 		"lewis6991/gitsigns.nvim",
 		event = { "BufReadPost", "BufNewFile", "BufWritePre", "BufEnter" },
