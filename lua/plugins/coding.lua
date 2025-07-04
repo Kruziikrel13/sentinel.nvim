53c2cee8b81e0e53afcf9adae9b9c6d2ea70c6fe
kruziikrel13 <dev@michaelpetersen.io>
2025-07-04 15:57:34 +1000

    coding: added coding file with lspsaga tools

---
+++ lua/plugins/coding.lua
@@ -0,0 +1,41 @@
+return {
+	{
+		"kruziikrel13/lspsaga.nvim",
+		event = "LspAttach",
+		dependencies = {
+			{ "nvim-treesitter/nvim-treesitter", optional = true },
+		},
+		opts = {
+			code_action = {
+				keys = {
+					quit = { "<Esc>", "q" },
+				},
+			},
+			finder = {
+				keys = {
+					vsplit = "<C-v>",
+					split = "<C-x>",
+					toggle_or_open = "<CR>",
+				},
+			},
+			rename = {
+				keys = {
+					quit = { "<Esc>", "q" },
+					select = "e",
+				},
+			},
+			lightbulb = {
+				sign = false,
+			},
+		},
+		keys = {
+			{ "<Tab><Tab>", "<CMD>Lspsaga hover_doc<CR>", desc = "Hover Doc" },
+			{ "<Tab>d", "<CMD>Lspsaga peek_definition<CR>", desc = "Peek Definition" },
+			{ "<Tab>f", "<CMD>Lspsaga finder<CR>", desc = "Find" },
+			{ "<Tab>a", "<CMD>Lspsaga code_action<CR>", desc = "Code Actions" },
+			{ "<leader>xx", "<CMD>Lspsaga show_workspace_diagnostics ++float<CR>", desc = "Workspace diagnostics" },
+			{ "<leader>xj", "<CMD>Lspsaga diagnostic_jump_next<CR>", desc = "Jump to next diagnostic" },
+			{ "<leader>xk", "<CMD>Lspsaga diagnostic_jump_prev<CR>", desc = "Jump to prev diagnostic" },
+		},
+	},
+}
