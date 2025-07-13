fb6296d45527488bf9e8f27299acb1e660fc0047
kruziikrel13 <dev@michaelpetersen.io>
2025-07-13 11:05:19 +1000

    dashboard: temporarily disable git terminals

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -42,86 +42,86 @@ return {
 					{ section = "header", align = "center" },
 					{ section = "keys", gap = 1, padding = 1 },
 				},
-				function()
-					if not Snacks.git.get_root() then
-						return
-					end
-
-					local gh = {
-						{
-							title = "Notifications",
-							indent = 3,
-							height = 5,
-							key = "N",
-							ttl = 5 * 60,
-							cmd = "gh notify -s -n5",
-							action = function()
-								vim.ui.open("https://github.com/notifications")
-							end,
-						},
-						{
-							title = "Open Issues",
-							indent = 3,
-							height = 7,
-							key = "I",
-							ttl = 5 * 60,
-							cmd = "gh issue list -L 3",
-							action = function()
-								vim.fn.jobstart("gh issue list --web", { detach = true })
-							end,
-						},
-						{
-							title = "Open PRs",
-							indent = 3,
-							height = 7,
-							key = "P",
-							ttl = 5 * 60,
-							cmd = "gh pr list -L 3",
-							action = function()
-								vim.fn.jobstart("gh pr list --web", { detach = true })
-							end,
-						},
-					}
-
-					local sections = {
-						{
-							icon = "",
-							title = "Browse Repo",
-							key = "b",
-							action = function()
-								---@diagnostic disable-next-line: missing-fields
-								Snacks.gitbrowse({
-									what = "repo",
-								})
-							end,
-						},
-						{
-							icon = "",
-							title = "Git Status",
-							indent = 3,
-							height = 5,
-							ttl = 5 * 60,
-							cmd = "git --no-pager diff --stat -B -M -C",
-							section = "terminal",
-						},
-					}
-
-					if vim.fn.executable("gh") == 1 then
-						gh = vim.tbl_map(function(cmd)
-							return vim.tbl_extend("keep", cmd, {
-								section = "terminal",
-							})
-						end, gh)
-						table.insert(sections, gh)
-					end
-
-					return vim.tbl_map(function(cmd)
-						return vim.tbl_extend("keep", cmd, {
-							pane = 2,
-							padding = 1,
-						})
-					end, sections)
-				end,
+				-- function()
+				-- 	if not Snacks.git.get_root() then
+				-- 		return
+				-- 	end
+				--
+				-- 	local gh = {
+				-- 		{
+				-- 			title = "Notifications",
+				-- 			indent = 3,
+				-- 			height = 5,
+				-- 			key = "N",
+				-- 			ttl = 5 * 60,
+				-- 			cmd = "gh notify -s -n5",
+				-- 			action = function()
+				-- 				vim.ui.open("https://github.com/notifications")
+				-- 			end,
+				-- 		},
+				-- 		{
+				-- 			title = "Open Issues",
+				-- 			indent = 3,
+				-- 			height = 7,
+				-- 			key = "I",
+				-- 			ttl = 5 * 60,
+				-- 			cmd = "gh issue list -L 3",
+				-- 			action = function()
+				-- 				vim.fn.jobstart("gh issue list --web", { detach = true })
+				-- 			end,
+				-- 		},
+				-- 		{
+				-- 			title = "Open PRs",
+				-- 			indent = 3,
+				-- 			height = 7,
+				-- 			key = "P",
+				-- 			ttl = 5 * 60,
+				-- 			cmd = "gh pr list -L 3",
+				-- 			action = function()
+				-- 				vim.fn.jobstart("gh pr list --web", { detach = true })
+				-- 			end,
+				-- 		},
+				-- 	}
+				--
+				-- 	local sections = {
+				-- 		{
+				-- 			icon = "",
+				-- 			title = "Browse Repo",
+				-- 			key = "b",
+				-- 			action = function()
+				-- 				---@diagnostic disable-next-line: missing-fields
+				-- 				Snacks.gitbrowse({
+				-- 					what = "repo",
+				-- 				})
+				-- 			end,
+				-- 		},
+				-- 		{
+				-- 			icon = "",
+				-- 			title = "Git Status",
+				-- 			indent = 3,
+				-- 			height = 5,
+				-- 			ttl = 5 * 60,
+				-- 			cmd = "git --no-pager diff --stat -B -M -C",
+				-- 			section = "terminal",
+				-- 		},
+				-- 	}
+				--
+				-- 	if vim.fn.executable("gh") == 1 then
+				-- 		gh = vim.tbl_map(function(cmd)
+				-- 			return vim.tbl_extend("keep", cmd, {
+				-- 				section = "terminal",
+				-- 			})
+				-- 		end, gh)
+				-- 		table.insert(sections, gh)
+				-- 	end
+				--
+				-- 	return vim.tbl_map(function(cmd)
+				-- 		return vim.tbl_extend("keep", cmd, {
+				-- 			pane = 2,
+				-- 			padding = 1,
+				-- 		})
+				-- 	end, sections)
+				-- end,
 				{ section = "startup" },
 			},
 			preset = { header = dashboard_icon },
