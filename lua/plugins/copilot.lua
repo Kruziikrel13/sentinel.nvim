33a449305532db0043ce98a2a6e2d0f1860db64a
kruziikrel13 <dev@michaelpetersen.io>
2025-07-14 13:13:07 +1000

    feat: switch to copilot chat plugin

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,16 +1,79 @@
 return (vim.g.ai and vim.fn.executable("copilot-language-server"))
 		and {
 			{
-				"olimorris/codecompanion.nvim",
-				cmd = { "CodeCompanion", "CodeCompanionActions", "CodeCompanionCmd", "CodeCompanionChat" },
-				opts = {
-					strategies = {
-						chat = {
-							adapter = "copilot",
+				"CopilotC-Nvim/CopilotChat.nvim",
+				branch = "main",
+				cmd = "CopilotChat",
+				opts = function()
+					local user = vim.env.USER or "User"
+					user = user:sub(1, 1):upper() .. user:sub(2)
+
+					return {
+						auto_insert_mode = true,
+						question_header = "  " .. user .. " ",
+						answer_header = "  Copilot ",
+						window = {
+							width = 0.4,
 						},
-						inline = {
-							adapter = "copilot",
-						},
+					}
+				end,
+				config = function(_, opts)
+					local chat = require("CopilotChat")
+					vim.api.nvim_create_autocmd("BufEnter", {
+						pattern = "copilot-chat",
+						callback = function()
+							vim.opt_local.relativenumber = false
+							vim.opt_local.number = false
+						end,
+					})
+
+					chat.setup(opts)
+				end,
+				keys = {
+					{
+						"<C-S>",
+						"<CR>",
+						ft = "copilot-chat",
+						desc = "Submit Prompt",
+						remap = true,
+					},
+					{
+						"<leader>cc",
+						function()
+							return require("CopilotChat").toggle()
+						end,
+						desc = "Toggle Copilot Chat",
+						mode = { "n", "v" },
+					},
+					{
+						"<leader>cx",
+						function()
+							return require("CopilotChat").reset()
+						end,
+						desc = "Clear Copilot Chat",
+						mode = { "n", "v" },
+					},
+					{
+						"<leader>cq",
+						function()
+							vim.ui.input({
+								prompt = "Quick Chat: ",
+							}, function(input)
+								if input ~= "" then
+									require("CopilotChat").ask(input)
+								end
+							end)
+						end,
+						desc = "Quick Chat",
+						mode = { "n", "v" },
+					},
+					{
+						"<leader>cp",
+						function()
+							require("CopilotChat").select_prompt()
+						end,
+						desc = "Prompt Actions",
+						mode = { "n", "v" },
 					},
 				},
 			},
@@ -18,7 +81,7 @@ return (vim.g.ai and vim.fn.executable("copilot-language-server"))
 				"zbirenbaum/copilot.lua",
 				cmd = "Copilot",
 				build = ":Copilot auth",
-				event = "InsertEnter",
+				event = "BufReadPre",
 				opts = {
 					suggestion = {
 						enabled = true,
@@ -28,6 +91,10 @@ return (vim.g.ai and vim.fn.executable("copilot-language-server"))
 						keymap = { accept = "<C-CR>", next = "<C-J>", prev = "<C-K>" },
 					},
 					panel = { enabled = false },
+					filetypes = {
+						markdown = true,
+						help = true,
+					},
 					server = { type = "binary", custom_server_filepath = "copilot-language-server" },
 				},
 			},
@@ -58,6 +125,14 @@ return (vim.g.ai and vim.fn.executable("copilot-language-server"))
 					},
 					keymap = {
 						["<A-CR>"] = { "select_and_accept", "show" },
+					},
+				},
+			},
+			{
+				"folke/which-key.nvim",
+				opts = {
+					spec = {
+						{ "<leader>c", group = "copilot", icon = { icon = "", color = "purple" } },
 					},
 				},
 			},
