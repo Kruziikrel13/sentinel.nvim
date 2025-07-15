e8c8a983cd9289f4aaffab38dd60645d40731473
kruziikrel13 <dev@michaelpetersen.io>
2025-07-16 09:13:36 +1000

    completions: use lsp_signature for signature help instead of blink

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,17 +1,28 @@
 return {
-	"saghen/blink.cmp",
-	dependencies = { "rafamadriz/friendly-snippets" },
-	version = "1.*",
-	opts = {
-		signature = { enabled = true },
-		completion = {
-			accept = { auto_brackets = { enabled = true } },
-			menu = { draw = { treesitter = { "lsp" } } },
+	{
+		"saghen/blink.cmp",
+		dependencies = { "rafamadriz/friendly-snippets" },
+		version = "1.*",
+		opts = {
+			completion = {
+				accept = { auto_brackets = { enabled = true } },
+				menu = { draw = { treesitter = { "lsp" } } },
+			},
+			keymap = {
+				["<A-CR>"] = { "select_and_accept" },
+				["<A-k>"] = { "select_prev" },
+				["<A-j>"] = { "select_next" },
+			},
 		},
-		keymap = {
-			["<A-CR>"] = { "select_and_accept" },
-			["<A-k>"] = { "select_prev" },
-			["<A-j>"] = { "select_next" },
+	},
+	{
+		"ray-x/lsp_signature.nvim",
+		event = "InsertEnter",
+		opts = {
+			bind = true,
+			handler_opts = {
+				border = "rounded",
+			},
 		},
 	},
 }
