2f0df51066a89a43cf0599590968b9853f71f201
kruziikrel13 <dev@michaelpetersen.io>
2025-08-15 10:19:21 +1000

    Revert "feat(dashboard): use alpha for dashboard instead of snacks"
    This reverts commit c5ac75d51bc578f5341cda1932b54380b74f128f.

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,63 +1,130 @@
+Sentinel.on_load("snacks.nvim", function()
+	-- Create highlights when snacks.nvim loads
+	vim.api.nvim_set_hl(0, "NeovimDashboardLogo1", { fg = "#641220" })
+	vim.api.nvim_set_hl(0, "NeovimDashboardLogo2", { fg = "#6e1423" })
+	vim.api.nvim_set_hl(0, "NeovimDashboardLogo3", { fg = "#85182a" })
+	vim.api.nvim_set_hl(0, "NeovimDashboardLogo4", { fg = "#a11d33" })
+	vim.api.nvim_set_hl(0, "NeovimDashboardLogo5", { fg = "#a71e34" })
+	vim.api.nvim_set_hl(0, "NeovimDashboardLogo6", { fg = "#b21e35" })
+	vim.api.nvim_set_hl(0, "NeovimDashboardLogo7", { fg = "#bd1f36" })
+	vim.api.nvim_set_hl(0, "NeovimDashboardLogo8", { fg = "#c71f37" })
+	vim.api.nvim_set_hl(0, "NeovimDashboardLogo9", { fg = "#da1e37" })
+end)
+
+local dashboard_icon = {
+	{ "        .*-.       \n", hl = "NeovimDashboardLogo1" },
+	{ "    -:   @+  .=    \n", hl = "NeovimDashboardLogo1" },
+	{ "  .*-    @=    #-  \n", hl = "NeovimDashboardLogo2" },
+	{ " +@-     @=     %*.\n", hl = "NeovimDashboardLogo2" },
+	{ "=@%:     @=     *@*\n", hl = "NeovimDashboardLogo3" },
+	{ " .+@#.   @=   =@*: \n", hl = "NeovimDashboardLogo3" },
+	{ "    =%*. @= -%*.   \n", hl = "NeovimDashboardLogo4" },
+	{ "      =%*@##+.     \n", hl = "NeovimDashboardLogo4" },
+	{ "       =@@@%-      \n", hl = "NeovimDashboardLogo5" },
+	{ "     +@#:@*=%#:    \n", hl = "NeovimDashboardLogo5" },
+	{ "  .+@%-  @+  =@#-  \n", hl = "NeovimDashboardLogo6" },
+	{ ".+@%-    @+   .+@#:\n", hl = "NeovimDashboardLogo6" },
+	{ "=@@=     @+    :#@*\n", hl = "NeovimDashboardLogo7" },
+	{ "  =%%-   @+  .*@*. \n", hl = "NeovimDashboardLogo7" },
+	{ "    =%%- @+ +@#:   \n", hl = "NeovimDashboardLogo8" },
+	{ "      =%%@%@#:     \n", hl = "NeovimDashboardLogo8" },
+	{ "        -#*:       ", hl = "NeovimDashboardLogo9" },
+}
+
 return {
-	{ "kruziikrel13/snacks.nvim", opts = { dashboard = { enabled = false } } },
-	{
-		"goolord/alpha-nvim",
-		dependencies = { "MaximilianLloyd/ascii.nvim" },
-		event = "VimEnter",
-		init = false,
-		opts = function()
-			local dashboard = require("alpha.themes.dashboard")
-			local ascii_categories = {
-				{ "computers", "apple" },
-				{ "computers", "atari" },
-				{ "computers", "linux" },
-				{ "computers", "consoles" },
-				{ "gaming", "doom" },
-				{ "gaming", "pacman" },
-				{ "misc", "skulls" },
-			}
-			math.randomseed(os.time() + math.random(1, 1000))
-			local category = ascii_categories[math.random(1, #ascii_categories)]
-			dashboard.section.header.val = require("ascii").get_random(unpack(category))
-
-			dashboard.section.buttons.val = {
-				dashboard.button("f", " " .. " Find file", "<cmd>lua Snacks.picker.files()<cr>"),
-				dashboard.button("n", " " .. " New file", "<cmd>ene <BAR> startinsert <cr>"),
-				dashboard.button("r", " " .. " Recent files", "<cmd>lua Snacks.picker.recent()<cr>"),
-				dashboard.button("g", " " .. " Find text", "<cmd>lua Snacks.picker.grep()<cr>"),
-				dashboard.button("s", " " .. " Restore Session", "<cmd>lua require('persistence').load()<cr>"),
-				dashboard.button("l", "󰒲 " .. " Lazy", "<cmd>Lazy<cr>"),
-				dashboard.button("q", " " .. " Quit", "<cmd>qa<cr>"),
-			}
-
-			-- vim.api.nvim_set_hl(0, "AlphaHeader", { fg = "#311B92" })
-			vim.api.nvim_set_hl(0, "AlphaButtons", { fg = "#D1C4E9" })
-			vim.api.nvim_set_hl(0, "AlphaShortcut", { fg = "#8BC34A" })
-			vim.api.nvim_set_hl(0, "AlphaFooter", { fg = "#edd691" })
-
-			for _, button in ipairs(dashboard.section.buttons.val) do
-				button.opts.hl = "AlphaButtons"
-				button.opts.hl_shortcut = "AlphaShortcut"
-			end
-			dashboard.section.header.opts.hl = "AlphaHeader"
-			dashboard.section.buttons.opts.hl = "AlphaButtons"
-			dashboard.section.footer.opts.hl = "AlphaFooter"
-			dashboard.opts.layout[1].val = 3
-			return dashboard
-		end,
-		config = function(_, dashboard)
-			-- close Lazy and re-open when the dashboard is ready
-			if vim.o.filetype == "lazy" then
-				vim.cmd.close()
-				vim.api.nvim_create_autocmd("User", {
-					once = true,
-					pattern = "AlphaReady",
-					callback = function()
-						require("lazy").show()
-					end,
-				})
-			end
-			require("alpha").setup(dashboard.opts)
-		end,
+	"kruziikrel13/snacks.nvim",
+	opts = {
+		dashboard = {
+			enable = true,
+			row = 6,
+			sections = {
+				{
+					{ section = "header", align = "center" },
+					{ section = "keys", gap = 1, padding = 1 },
+				},
+				-- function()
+				-- 	if not Snacks.git.get_root() then
+				-- 		return
+				-- 	end
+				--
+				-- 	local gh = {
+				-- 		{
+				-- 			title = "Notifications",
+				-- 			indent = 3,
+				-- 			height = 5,
+				-- 			key = "N",
+				-- 			ttl = 5 * 60,
+				-- 			cmd = "gh notify -s -n5",
+				-- 			action = function()
+				-- 				vim.ui.open("https://github.com/notifications")
+				-- 			end,
+				-- 		},
+				-- 		{
+				-- 			title = "Open Issues",
+				-- 			indent = 3,
+				-- 			height = 7,
+				-- 			key = "I",
+				-- 			ttl = 5 * 60,
+				-- 			cmd = "gh issue list -L 3",
+				-- 			action = function()
+				-- 				vim.fn.jobstart("gh issue list --web", { detach = true })
+				-- 			end,
+				-- 		},
+				-- 		{
+				-- 			title = "Open PRs",
+				-- 			indent = 3,
+				-- 			height = 7,
+				-- 			key = "P",
+				-- 			ttl = 5 * 60,
+				-- 			cmd = "gh pr list -L 3",
+				-- 			action = function()
+				-- 				vim.fn.jobstart("gh pr list --web", { detach = true })
+				-- 			end,
+				-- 		},
+				-- 	}
+				--
+				-- 	local sections = {
+				-- 		{
+				-- 			icon = "",
+				-- 			title = "Browse Repo",
+				-- 			key = "b",
+				-- 			action = function()
+				-- 				---@diagnostic disable-next-line: missing-fields
+				-- 				Snacks.gitbrowse({
+				-- 					what = "repo",
+				-- 				})
+				-- 			end,
+				-- 		},
+				-- 		{
+				-- 			icon = "",
+				-- 			title = "Git Status",
+				-- 			indent = 3,
+				-- 			height = 5,
+				-- 			ttl = 5 * 60,
+				-- 			cmd = "git --no-pager diff --stat -B -M -C",
+				-- 			section = "terminal",
+				-- 		},
+				-- 	}
+				--
+				-- 	if vim.fn.executable("gh") == 1 then
+				-- 		gh = vim.tbl_map(function(cmd)
+				-- 			return vim.tbl_extend("keep", cmd, {
+				-- 				section = "terminal",
+				-- 			})
+				-- 		end, gh)
+				-- 		table.insert(sections, gh)
+				-- 	end
+				--
+				-- 	return vim.tbl_map(function(cmd)
+				-- 		return vim.tbl_extend("keep", cmd, {
+				-- 			pane = 2,
+				-- 			padding = 1,
+				-- 		})
+				-- 	end, sections)
+				-- end,
+				{ section = "startup" },
+			},
+			preset = { header = dashboard_icon },
+		},
 	},
 }
