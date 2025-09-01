6ec84804dc32bfc9ef0c51b3e88c84ba3b8d18d2
kruziikrel13 <dev@michaelpetersen.io>
2025-09-02 00:04:05 +1000

    feat: switch back to nvim-tree for explorer

--- lua/plugins/explorer.lua
+++ lua/plugins/explorer.lua
@@ -1,54 +1,61 @@
+local function my_on_attach(bufnr)
+	local api = require("nvim-tree.api")
+	api.config.mappings.default_on_attach(bufnr)
+
+	local function opts(desc)
+		return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+	end
+
+	vim.keymap.set("n", ">", api.tree.change_root_to_node, opts("change root to node"))
+	vim.keymap.set("n", "<", api.tree.change_root_to_parent, opts("change root to parent"))
+	vim.keymap.set("n", "<BS>", api.tree.toggle_hidden_filter, opts("toggle hidden"))
+	vim.keymap.set("n", "g<BS>", api.tree.toggle_gitignore_filter, opts("toggle gitignore"))
+	vim.keymap.set("n", "c", api.node.navigate.parent_close, opts("close parent"))
+	vim.keymap.set("n", "C", api.tree.collapse_all, opts("close parent"))
+end
+
 return {
-	"kruziikrel13/snacks.nvim",
-	opts = {
-		explorer = { enable = true },
-		picker = {
-			sources = {
-				explorer = {
-					icons = {
+	{
+		"nvim-tree/nvim-tree.lua",
+		opts = {
+			on_attach = my_on_attach,
+			hijack_cursor = true,
+			disable_netrw = true,
+			prefer_startup_root = true,
+			sync_root_with_cwd = true,
+			modified = {
+				enable = true,
+			},
+			renderer = {
+				group_empty = true,
+				hidden_display = "simple",
+				icons = {
+					git_placement = "after",
+					glyphs = {
 						git = {
-							added = Sentinel.config.icons.git.added,
+							unstaged = Sentinel.config.icons.git.modified,
 							staged = Sentinel.config.icons.git.staged,
-							modified = Sentinel.config.icons.git.modified,
+							untracked = Sentinel.config.icons.git.untracked,
 							deleted = Sentinel.config.icons.git.removed,
-						},
-					},
-					win = {
-						input = {
-							keys = {
-								["<Esc>"] = {
-									function()
-										vim.cmd.wincmd("p")
-									end,
-									mode = { "n", "i" },
-								},
-							},
-						},
-						list = {
-							keys = {
-								["<BS>"] = "toggle_hidden",
-								["C"] = "explorer_close_all",
-								["E"] = "explorer_open_all", -- From PR https://github.com/folke/snacks.nvim/pull/1652
-								["c"] = "explorer_close",
-								["<"] = "explorer_up",
-								[">"] = "explorer_focus",
-								["<C-W><C-W>"] = function()
-									vim.cmd.wincmd("l")
-								end,
-							},
 						},
 					},
 				},
 			},
+			diagnostics = {
+				enable = true,
+			},
+			filters = {
+				dotfiles = true,
+			},
+		},
+		lazy = false,
+		keys = {
+			{ "t", "<CMD>NvimTreeToggle<cr>", desc = "Explorer" },
 		},
 	},
-	keys = {
-		{
-			"t",
-			function()
-				Snacks.explorer()
-			end,
-			{ desc = "File Explorer" },
-		},
+	{
+		"antosha417/nvim-lsp-file-operations",
+		config = true,
+		dependencies = { "nvim-tree/nvim-tree.lua", "nvim-lua/plenary.nvim" },
 	},
 }
