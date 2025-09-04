9f82aa89fb581dc51ed19e1423ef70e064d4bb41
kruziikrel13 <dev@michaelpetersen.io>
2025-09-04 13:56:54 +1000

    completions: remove lsp_signature
    duplicate hover windows are created with blink already

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -15,14 +15,4 @@ return {
 			},
 		},
 	},
-	{
-		"ray-x/lsp_signature.nvim",
-		event = "InsertEnter",
-		opts = {
-			bind = true,
-			handler_opts = {
-				border = "rounded",
-			},
-		},
-	},
 }
