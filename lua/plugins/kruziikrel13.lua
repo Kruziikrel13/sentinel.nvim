8276e022b8c6f22dc71eb81e7d9a956a7921d8ba
kruziikrel13 <dev@michaelpetersen.io>
2025-09-15 17:52:12 +1000

    use master branch of snacks

--- lua/plugins/kruziikrel13.lua
+++ lua/plugins/kruziikrel13.lua
@@ -7,7 +7,6 @@ return {
 	{
 		-- snacks with various pr's merged and custom changes
 		"kruziikrel13/snacks.nvim",
-		branch = "pr_merges",
 	},
 	{
 		-- lspsaga with functionality stripped out that I don't use
