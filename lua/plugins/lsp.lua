8d38464b94c3172f3c9572882d4a978c7837b501
kruziikrel13 <dev@michaelpetersen.io>
2025-10-31 11:10:57 +1000

    work

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,8 +2,15 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		event = { "BufReadPost", "BufNewFile", "BufWritePre" },
-		config = function()
-			vim.diagnostic.config({
+		opts_extend = { "servers.*.keys" },
+		opts = {
+			diagnostics = {
+				underline = true,
+				update_in_insert = false,
+				virtual_text = {
+					spacing = 4,
+				},
+				severity_sort = true,
 				signs = {
 					text = {
 						[vim.diagnostic.severity.ERROR] = Sentinel.config.icons.diagnostics.Error,
@@ -12,8 +19,105 @@ return {
 						[vim.diagnostic.severity.INFO] = Sentinel.config.icons.diagnostics.Info,
 					},
 				},
-			})
-		end,
+			},
+			inlay_hints = {
+				enabled = true,
+				exclude = {}, -- file types to exclude inlay hints
+			},
+			codelens = { -- enable builtin lsp code lenses
+				enabled = false,
+			},
+			folds = {
+				enabled = true,
+			},
+			servers = {
+				["*"] = {
+					workspace = {
+						fileOperations = {
+							didRename = true,
+							willRename = true,
+						},
+					},
+				},
+				lua_ls = {
+					settings = {
+						Lua = {
+							workspace = {
+								checkThirdParty = false,
+							},
+							codeLens = {
+								enable = true,
+							},
+							completion = {
+								callSnippet = "Replace",
+							},
+							doc = {
+								privateName = { "^_" },
+							},
+							hint = {
+								enable = true,
+								setType = false,
+								paramType = true,
+								paramName = "Disable",
+								semicolon = "Disable",
+								arrayIndec = "Disable",
+							},
+						},
+					},
+				},
+			},
+		},
+		config = vim.schedule_wrap(function(_, opts)
+			if opts.inlay_hints.enabled then
+				Snacks.util.lsp.on({ method = "textDocument/inlayHint" }, function(buf)
+					if
+						vim.api.nvim_buf_is_valid(buf)
+						and vim.bo[buf].buftype
+						and not vim.tbl_contains(opts.inlay_hints.exclude, vim.bo[buf].filetype)
+					then
+						vim.lsp.inlay_hint.enable(true, { bufnr = buf })
+					end
+				end)
+			end
+
+			if opts.folds.enabled then
+				Snacks.util.lsp.on({ method = "textDocument/foldingRange" }, function()
+					if vim.o.foldmethod == "expr" then
+						vim.o.foldexpr = "v:lua.vim.lsp.foldexpr()"
+					end
+				end)
+			end
+
+			if opts.codelens.enabled and vim.lsp.codelens then
+				Snacks.util.lsp.on({ method = "textDocument/codeLens" }, function(buf)
+					vim.lsp.codelens.refresh()
+					vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "InsertLeave" }, {
+						buffer = buf,
+						callback = vim.lsp.codelens.refresh,
+					})
+				end)
+			end
+
+			vim.diagnostic.config(vim.deepcopy(opts.diagnostics))
+
+			if opts.servers["*"] then
+				vim.lsp.config("*", opts.servers["*"])
+			end
+
+			local function configure(server)
+				if server == "*" then
+					return false
+				end
+				local sopts = opts.servers[server]
+				sopts = sopts == true and {} or (not sopts) and { enabled = false } or sopts
+
+				local setup = opts.setup[server] or opts.setup["*"]
+				if not setup and setup(server, sopts) then
+					vim.lsp.config(server, sopts)
+					vim.lsp.enable(server)
+				end
+			end
+		end),
 	},
 	{
 		"kruziikrel13/lspsaga.nvim",
--- lua/util/lang.lua
+++ lua/util/lang.lua
@@ -1,3 +1,13 @@
 local M = {}
 
+M._supported = {
+	"c",
+	"cpp",
+	"rust",
+	"lua",
+	"nix",
+}
+
+function M.setup() end
+
 return M
