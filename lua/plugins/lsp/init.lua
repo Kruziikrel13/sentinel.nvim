f98b2eea6b897d725be9eaf3928acb3a0320074d
kruziikrel13 <dev@michaelpetersen.io>
2025-11-09 21:56:40 +1000

    lsp: migrate to plugins directory

---
+++ lua/plugins/lsp/init.lua
@@ -0,0 +1,25 @@
+return {
+	"neovim/nvim-lspconfig",
+	event = "LazyFile",
+	opts = {
+		---@type vim.diagnostic.Opts
+		diagnostics = {
+			underline = true,
+			update_in_insert = false,
+			virtual_lines = true,
+			virtual_text = false,
+			severity_sort = true,
+			signs = {
+				text = {
+					[vim.diagnostic.severity.ERROR] = Sentinel.config.icons.diagnostics.Error,
+					[vim.diagnostic.severity.WARN] = Sentinel.config.icons.diagnostics.Warn,
+					[vim.diagnostic.severity.HINT] = Sentinel.config.icons.diagnostics.Hint,
+					[vim.diagnostic.severity.INFO] = Sentinel.config.icons.diagnostics.Info,
+				},
+			},
+		},
+	},
+	config = vim.schedule_wrap(function(_, opts)
+		vim.diagnostic.config(vim.deepcopy(opts.diagnostics))
+	end),
+}
