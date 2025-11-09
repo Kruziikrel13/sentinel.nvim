e40d4124905d18a66eb899a66af554c654fb5bea
kruziikrel13 <dev@michaelpetersen.io>
2025-11-09 22:16:44 +1000

    plugins: refactor init.lua

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -17,17 +17,5 @@ return {
 		priority = 1000,
 		lazy = false,
 		opts = {},
-		keys = {
-			{ "<leader>L", "<CMD>Lazy<CR>", desc = "Lazy" },
-		},
-		config = function(_, opts)
-			local notify = vim.notify
-			require("snacks").setup(opts)
-			-- HACK: restore vim.notify after snacks setup and let noice.nvim take over
-			-- this is needed to have early notifications show up in noice history
-			if Sentinel.has("noice.nvim") then
-				vim.notify = notify
-			end
-		end,
 	},
 }
