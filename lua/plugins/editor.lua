d6a82b1c0ce5f3bec2f6a855ab52e933bc14f284
kruziikrel13 <dev@michaelpetersen.io>
2025-11-20 15:40:31 +1000

    fix: additional which-key definitions

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -12,11 +12,23 @@ return {
 					{ "<leader>f", group = "file/find" },
 					{ "<leader>q", group = "quit/session" },
 					{ "<leader>u", group = "utilities" },
+					{ "<leader>b", group = "buffers" },
+					{ "<leader>w", group = "windows" },
+					{ "<leader><Tab>", group = "tabs" },
 					{ "[", group = "prev" },
 					{ "]", group = "next" },
 					{ "g", group = "goto" },
 					{ "gs", group = "surround" },
 				},
+			},
+		},
+		keys = {
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "Buffer Keymaps (which-key)",
 			},
 		},
 	},
