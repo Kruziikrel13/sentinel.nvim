39785bd742be9b3aac3caf8226d3a245011cdfe4
kruziikrel13 <dev@michaelpetersen.io>
2025-11-24 11:47:17 +1000

    add tiny glimmer anims

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -68,4 +68,15 @@ return {
 			},
 		},
 	},
+
+	{
+		"rachartier/tiny-glimmer.nvim",
+		event = "VeryLazy",
+		priority = 10,
+		opts = {
+			search = { enabled = true },
+			undo = { enabled = true },
+			redo = { enabled = true },
+		},
+	},
 }
