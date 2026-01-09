aaea5c0368cc1b8e545dd6ef2b87039fe0caa6a9
kruziikrel13 <dev@michaelpetersen.io>
2026-01-09 10:57:56 +1000

    colorschemes: added and set teide scheme

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 vim.uv = vim.uv or vim.loop
 
 vim.g.ENABLE_SIDEKICK = true
+vim.g.EDITOR_THEME = "teide"
 
 require("loader")
---
+++ lua/plugins/colorschemes/teide.lua
@@ -0,0 +1,8 @@
+return {
+	---@see https://github.com/serhez/teide.nvim
+	"serhez/teide.nvim",
+	init = function()
+		vim.cmd.colorscheme("teide-darker")
+	end,
+	opts = {},
+}
