06fe8d82a8862e81e6e5d2ecdc179aa8f5aa7828
kruziikrel13 <dev@michaelpetersen.io>
2026-01-22 20:24:42 +1000

    rust: add rust dap

--- lua/plugins/lsp/languages/rust.lua
+++ lua/plugins/lsp/languages/rust.lua
@@ -1,7 +1,4 @@
-if not Sentinel.executable({ "bacon", "rust-analyzer" }) then
-	return {}
-end
-
+local diagnostics = "rust-analyzer"
 return {
 	{
 		"nvim-treesitter",
@@ -11,17 +8,80 @@ return {
 		"nvim-lspconfig",
 		opts = {
 			servers = {
-				bacon_ls = {},
-				rust_analyzer = {},
+				rust_analyzer = { enabled = false },
 			},
 		},
 	},
 	{
-		"mrcjkb/rustaceanvim",
-		ft = "rust",
+		"nvim-dap",
+		opts = {
+			adapters = {
+				lldb = {
+					type = "executable",
+					command = "lldb-dap",
+					name = "lldb",
+				},
+			},
+			configurations = {
+				rust = {
+					{
+						name = "Launch",
+						type = "lldb",
+						request = "launch",
+						program = function()
+							return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/target/debug", "file")
+						end,
+						cwd = "${workspaceFolder}",
+						args = {},
+					},
+				},
+			},
+		},
 	},
 	{
-		"alexpasmantier/krust.nvim",
+		"theHamsta/nvim-dap-virtual-text",
+		opts = {},
+	},
+	{
+		"mrcjkb/rustaceanvim",
 		ft = "rust",
+		opts = {
+			server = {
+				on_attach = function(_, bufnr)
+					vim.keymap.set("n", "<Tab>a", function()
+						vim.cmd.RustLsp("codeAction")
+					end, { desc = "Code Action", buffer = bufnr })
+				end,
+				default_settings = {
+					["rust-analyzer"] = {
+						cargo = {
+							allFeatures = true,
+							loadOutDirsFromCheck = true,
+							buildScripts = {
+								enable = true,
+							},
+						},
+						-- Add clippy lints for Rust if using rust-analyzer
+						checkOnSave = diagnostics == "rust-analyzer",
+						-- Enable diagnostics if using rust-analyzer
+						diagnostics = {
+							enable = diagnostics == "rust-analyzer",
+						},
+						procMacro = {
+							enable = true,
+						},
+						files = {
+							watcher = "client",
+						},
+					},
+				},
+			},
+		},
+		config = function(_, opts)
+			vim.g.rustaceanvim = vim.tbl_deep_extend("keep", vim.g.rustaceanvim or {}, opts or {})
+			if not Sentinel.executable("rust-analyzer") then
+				Sentinel.error("**rust-analyzer** not found in PATH")
+			end
+		end,
 	},
 }
