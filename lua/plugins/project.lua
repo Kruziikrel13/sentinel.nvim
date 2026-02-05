cc5989d861dada07607ecab075da510efcab5026
kruziikrel13 <dev@michaelpetersen.io>
2026-02-05 19:53:36 +1000

    Revert "remove project cfg"
    This reverts commit e2bce115f82fe3aa7dda460326555ce21cf0d240.

---
+++ lua/plugins/project.lua
@@ -0,0 +1,17 @@
+return {
+	{
+		"DrKJeff16/project.nvim",
+		opts = {},
+	},
+	{
+		"nvim-tree/nvim-tree.lua",
+		optional = true,
+		opts = {
+			respect_buf_cwd = true,
+			update_focused_file = {
+				enable = true,
+				update_root = true,
+			},
+		},
+	},
+}
