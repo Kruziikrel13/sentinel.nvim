c6904231eeaadba7a413a0904788fc2b1251b1cb
kruziikrel13 <dev@michaelpetersen.io>
2026-03-17 09:32:29 +1000

    work

--- flake.nix
+++ flake.nix
@@ -4,22 +4,31 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nvim-nightly.url = "github:nix-community/neovim-nightly-overlay";
     nvim-nightly.inputs.nixpkgs.follows = "nixpkgs";
+    nvf.url = "github:NotAShelf/nvf";
   };
   outputs =
     {
       self,
       nixpkgs,
       nvim-nightly,
+      nvf,
     }:
     let
       inherit (nixpkgs) lib;
-      overlayPkgs = p: p.appendOverlays [ self.overlays.default ];
+      overlayPkgs = p: p.appendOverlays [ nvim-nightly.overlays.default ];
       systems = lib.platforms.linux;
       forEachSystem =
         fn: lib.genAttrs systems (system: fn system (overlayPkgs nixpkgs.legacyPackages.${system}));
     in
     {
-      overlays.default = import ./nix/overlay.nix nvim-nightly;
+      packages = forEachSystem (
+        system: pkgs: {
+          sentinel = pkgs.callPackage ./nix/package.nix {
+            neovim-unwrapped = nvim-nightly.packages.${system}.default;
+          };
+          nvf = pkgs.callPackage ./nix/nvf.nix { inherit nvf pkgs; };
+        }
+      );
 
       devShells = forEachSystem (
         system: pkgs: {
@@ -27,12 +36,6 @@
             inherit pkgs;
             inherit (self.packages.${system}) sentinel;
           };
-        }
-      );
-      packages = forEachSystem (
-        system: pkgs: rec {
-          inherit (pkgs) sentinel;
-          default = sentinel;
         }
       );
     };
